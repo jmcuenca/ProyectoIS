22f4bfb30591afaed52bc0041a1cf20e10f69f73
kipuxdev <info@kipuxdev.com>
2020-01-30 16:37:19 -0500

    Actualización inicial del proyecto

---
+++ Proyecto/WebApiCore/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using WebApiCore.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApiCore.Controllers
+{
+    [AuthActionFilter]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Charts()
+        {
+            return View();
+        }
+
+        public ActionResult Tables()
+        {
+            return View();
+        }
+
+        public ActionResult Forms()
+        {
+            return View();
+        }
+
+        public ActionResult BootstrapElements()
+        {
+            return View();
+        }
+
+        public ActionResult BootstrapGrid()
+        {
+            return View();
+        }
+
+        public ActionResult BlankPage()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Proyecto/WebApiCore/Controllers/NavbarController.cs
@@ -0,0 +1,23 @@
+﻿using WebApiCore.Domain;
+using WebApiCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApiCore.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Navbar(string controller, string action)
+        {
+            var data = new Data();
+
+            var navbar = data.itemsPerUser(controller, action, User.Identity.Name);
+
+            return PartialView("_navbar", navbar);
+        }
+    }
+}
